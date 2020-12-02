44f4511945c212cd42e3879995acf5c2725983f4
Tim Flood <tflood9@gmail.com>
2020-12-02 12:15:34 -0500

    finish signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
+  get '/signup' do
+    erb :"users/new.html"
+  end
 
+  post '/users' do
+    @user = User.new
+    @user.email = params[:email]
+    @user.password = params[:password]
+    if @user.save
+      redirect '/login'
+    else
+      erb :"users/new.html"
+    end
+  end
 end
